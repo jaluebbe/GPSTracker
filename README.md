e7656735b463d1d7ce3d5529b6baef2492ae0fab
jannis <jluebbe@uos.de>
2019-10-25 20:59:15 +0200

    added GPS track visualisation examples.

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # GPSTracker
-Use a Raspberry Pi with a GPS receiver and a pressure sensor
+Use a Raspberry Pi with a GPS receiver and a pressure sensor. 
+Tracking data from this setup and other sources can be visualised using 
+Leaflet.js and several plugins. 
+
+## GPS tracking
+The code for GPS tracking using a Raspberry Pi will follow later.
+
+## GPS track visualisation
+Different data sources are visualised on 
+https://jaluebbe.github.io/GPSTracker/ .
+The artificial data was created using geojson.io. 
+The real data is taken from a Raspberry Pi equipped with GPS and barometer. 
+The Airbus tree is taken from the sample data supplied with 
+https://github.com/xoolive/traffic .
+Elevation data was determined using the method published in 
+https://github.com/jaluebbe/HeightMap .
---
+++ gps_visualisation.js
@@ -0,0 +1,103 @@
+var map = L.map('map').setView([50.0, 8.0], 5);
+map.attributionControl.addAttribution(
+    '<a href="https://github.com/jaluebbe/GPSTracker">Source on GitHub</a>');
+// add link to privacy statement
+//map.attributionControl.addAttribution(
+//    '<a href="static/datenschutz.html" target="_blank">Datenschutzerkl&auml;rung</a>');
+var wmsLayer = L.tileLayer.wms('https://sgx.geodatenzentrum.de/wms_topplus_open', {
+    layers: 'web',
+    format: 'image/png',
+    transparent: true,
+    minZoom: 1,
+    attribution: '&copy <a href="https://www.bkg.bund.de">BKG</a> 2019, ' +
+        '<a href= "http://sg.geodatenzentrum.de/web_public/Datenquellen_TopPlus_Open.pdf" >data sources</a> '
+});
+var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+var otmLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 15,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> ' +
+        'contributors, SRTM | Map style: &copy; ' +
+        '<a href="https://opentopomap.org">OpenTopoMap</a> ' +
+        '(<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+esriImagery = L.layerGroup([
+    L.esri.basemapLayer('Imagery', {
+        maxZoom: 18
+    }),
+    L.esri.basemapLayer('ImageryLabels', {
+        maxZoom: 18
+    })
+]);
+L.control.scale({
+    'imperial': false
+}).addTo(map);
+var legend = L.control({
+    position: 'topright'
+});
+legend.onAdd = function(map) {
+    var div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML =
+        '<table><tr><td>Choose data</td><td><select id="trackSelect">' +
+        '<option selected value="artificial_tracking_data.json">artificial data</option>' +
+        '<option value="real_tracking_data.json">real data</option>' +
+        '<option value="airbus_tree.json">Airbus tree</option>' +
+        '</select></td></tr>' +
+        '</table>';
+    L.DomEvent.on(div, 'click', function(ev) {
+        L.DomEvent.stopPropagation(ev);
+    });
+
+    return div;
+};
+
+legend.addTo(map);
+
+
+baseLayers = {
+    "TopPlusOpen": wmsLayer,
+    "OpenTopoMap": otmLayer,
+    "OpenStreetMap": osmLayer,
+    "Esri Imagery": esriImagery
+};
+otherLayers = {};
+var layerControl = L.control.layers(baseLayers, otherLayers, {
+    collapsed: L.Browser.mobile, // hide on mobile devices
+    position: 'topright'
+}).addTo(map);
+
+var hg = L.control.heightgraph({
+    width: 480,
+    height: 180,
+    margins: {
+        top: 10,
+        right: 30,
+        bottom: 55,
+        left: 50
+    },
+    expand: false,
+    position: "bottomright"
+});
+hg.addTo(map);
+var geoJsonLayer = L.geoJson([]).addTo(map);
+
+function loadGeoJSON(fileName) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', fileName);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            var geojson = JSON.parse(xhr.responseText);
+            hg.addData(geojson);
+            geoJsonLayer.clearLayers();
+            geoJsonLayer.addData(geojson);
+            map.fitBounds(geoJsonLayer.getBounds());
+        }
+    };
+    xhr.send();
+}
+loadGeoJSON(document.getElementById("trackSelect").value);
+document.getElementById("trackSelect").onchange = function() {
+    loadGeoJSON(document.getElementById("trackSelect").value);
+};
