5c579a6e4ed97cfb981d8f29b03be22122a40d93
Jannis Lübbe <jluebbe@uos.de>
2020-07-17 17:22:48 +0200

    pressure logging is optional. Strong pressure deviations will be logged separately.

--- gps_tracker/pressure_logger.py
+++ gps_tracker/pressure_logger.py
@@ -8,18 +8,25 @@ from collections import deque
 from time import localtime, sleep, strftime, time, strptime
 pressure_history = deque(maxlen=500)
 _pubsub = redis_connection.pubsub()
-_pubsub.subscribe(['bmp280'])
+_pubsub.subscribe(['bmp280', 'transfer_data'])
 #ca 17/min with 0.05s
 last_record = None
 old_pressure = None
+last_transfer = None
+log_pressure = False
 
 for item in _pubsub.listen():
     if not item[u'type'] == 'message':
         continue
+    if item['channel'] == 'transfer_data':
+        _transfer_data = json.loads(item['data'])
+        last_transfer = _transfer_data['utc']
+        old_pressure = _transfer_data['pressure']
+        continue
     data = json.loads(item['data'])
     pressure_history.append(data['pressure'])
     now = time()
-    if last_record is None or last_record < now - 5:
+    if log_pressure and (last_record is None or last_record < now - 5):
         pressure_data = {
             'pressure': int(round(np.mean(pressure_history))),
             'temperature': round(data['temperature'], 1),
