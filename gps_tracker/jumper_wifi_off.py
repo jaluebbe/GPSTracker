bba00ecda84638927aa057ec446f8508c0c77643
Jannis Lübbe <jluebbe@uos.de>
2020-09-07 17:27:49 +0200

    added option to switch of wifi via jumper between GND and GPIO21. Cannot be used simultaneously with button shutdown option on the same GPIO port.

---
+++ gps_tracker/jumper_wifi_off.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import time
+import os
+from syslog import syslog
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)  # Set pin numbering to board numbering
+GPIO.setup(21, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Setup pin 21 as an input
+
+if __name__ == '__main__':
+    wifi_disabled = False
+    while True:
+        if GPIO.input(21) == 0 and not wifi_disabled:
+            os.system("sudo iwconfig wlan0 txpower off")
+            wifi_disabled = True
+            syslog("disabled wifi")
+        elif GPIO.input(21) == 1 and wifi_disabled:
+            os.system("sudo iwconfig wlan0 txpower auto")
+            wifi_disabled = False
+            syslog("enabled wifi")
+        # reduce CPU usage by sleep time
+        time.sleep(10.0)
