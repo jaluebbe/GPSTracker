12839bc4c3615e8f32bf3c3703ee488af1b4f631
Jannis Lübbe <jluebbe@uos.de>
2020-09-14 20:24:28 +0200

    added some error handling to deal with failed rpm/temperature sensors.

--- gps_tracker/ulf2_poller.py
+++ gps_tracker/ulf2_poller.py
@@ -13,16 +13,28 @@ redis_connection = redis.Redis()
 def poll_ulf2():
     utc = time.time()
     ulf2_data = {}
-    temperatures = requests.get(temperatures_url, timeout=0.1).json()
-    if temperatures.get('source') == 'ulf2_temperatures':
-        del temperatures['source']
-        del temperatures['time']
-        ulf2_data.update(temperatures)
-    rpm = requests.get(rpm_url, timeout=0.1).json()
-    if rpm.get('source') == 'ulf2_rpm':
-        del rpm['source']
-        del rpm['time']
-        ulf2_data.update(rpm)
+    try:
+        temperatures = requests.get(temperatures_url, timeout=0.1).json()
+    except requests.exceptions.ConnectTimeout:
+        print(f'connection to {temperatures_url} timed out.')
+    except requests.exceptions.ConnectionError:
+        print(f'connection to {temperatures_url} failed.')
+    else:
+        if temperatures.get('source') == 'ulf2_temperatures':
+            del temperatures['source']
+            del temperatures['time']
+            ulf2_data.update(temperatures)
+    try:
+        rpm = requests.get(rpm_url, timeout=0.1).json()
+    except requests.exceptions.ConnectTimeout:
+        print(f'connection to {rpm_url} timed out.')
+    except requests.exceptions.ConnectionError:
+        print(f'connection to {rpm_url} failed.')
+    else:
+        if rpm.get('source') == 'ulf2_rpm':
+            del rpm['source']
+            del rpm['time']
+            ulf2_data.update(rpm)
     if len(ulf2_data) > 0:
         ulf2_data['utc'] = round(utc, 1)
         redis_connection.set('ulf2', json.dumps(ulf2_data))
