f9f662a61cf97ea4cebf57e92a8e2823900ba3eb
Jannis Lübbe <jluebbe@uos.de>
2021-08-02 21:39:59 +0200

    replaced constant by default parameter p0.

--- gps_tracker/backend_fastapi.py
+++ gps_tracker/backend_fastapi.py
@@ -13,7 +13,7 @@ app.mount("/static", StaticFiles(directory="../static"), name="static")
 # calculate barometric altitude based on the following formula:
 # https://www.weather.gov/media/epz/wxcalc/pressureAltitude.pdf
 def calculate_pressure_altitude(pressure, p0=101_325):
-    altitude = 0.3048 * 145_366.45 * (1 - pow(pressure / 101_325, 0.190_284))
+    altitude = 0.3048 * 145_366.45 * (1 - pow(pressure / p0, 0.190_284))
     return altitude
 
 
