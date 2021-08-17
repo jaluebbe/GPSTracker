a5ec80078dc9d4adb2675e2e2b490fc78e0a2996
Jannis Lübbe <jluebbe@uos.de>
2021-08-18 00:13:38 +0200

    optimized pressure_logger.py to be more suitable for recording calibration data.

--- gps_tracker/pressure_logger.py
+++ gps_tracker/pressure_logger.py
@@ -1,54 +1,64 @@
 #!/usr/bin/env python3
-import redis
+import time
 import json
-import os
+from collections import deque
 import numpy as np
+import redis
 
 redis_connection = redis.Redis(decode_responses=True)
-from collections import deque
-from time import localtime, sleep, strftime, time, strptime
-
-pressure_history = deque(maxlen=500)
+pressure_buffer = deque(maxlen=700)
+buffer_time = None
 _pubsub = redis_connection.pubsub()
 _pubsub.subscribe(["bmp280", "bme280", "bmp388", "transfer_data"])
-# ca 17/min with 0.05s
-last_record = 0
-last_log = 0
 old_pressure = 0
-last_transfer = None
-log_altitude = False
-log_pressure = False
-
+log_altitude = True
+log_pressure = True
+log_pressure_minutes = [20, 50]
 
 for item in _pubsub.listen():
     if not item[u"type"] == "message":
         continue
+    # the last transfer_data message represents the last data dump to Redis.
     if item["channel"] == "transfer_data":
         _transfer_data = json.loads(item["data"])
-        last_transfer = _transfer_data["utc"]
         old_pressure = _transfer_data["pressure"]
         continue
     data = json.loads(item["data"])
-    pressure_history.append(data["pressure"])
-    now = time()
-    if last_record < now - 5:
+    utc = data["p_utc"]
+    utc_ceil_minute = utc - utc % 60 + 60
+    next_minute = time.gmtime(utc_ceil_minute).tm_min
+    if buffer_time is None:
+        buffer_time = utc_ceil_minute
+    elif utc_ceil_minute > buffer_time and len(pressure_buffer) > 0:
         pressure_data = {
-            "pressure": int(round(np.mean(pressure_history))),
+            "pressure": int(round(np.mean(pressure_buffer))),
             "temperature": round(data["temperature"], 1),
-            "utc": int(round(now)),
+            "utc": int(buffer_time),
             "hostname": data["hostname"],
         }
         if item["channel"] == "bme280":
             pressure_data["humidity"] = int(round(data["humidity"]))
-        if log_pressure and last_log < now - 60:
-            key = "pressure:{}:{}".format(data["hostname"], strftime("%Y%m"))
-            redis_connection.lpush(key, json.dumps(pressure_data))
-            last_log = now
         redis_connection.set("current_pressure", json.dumps(pressure_data))
-        last_record = now
+        if (
+            log_pressure
+            and time.gmtime(buffer_time).tm_min in log_pressure_minutes
+        ):
+            key = "pressure:{}:{}".format(
+                data["hostname"], time.strftime("%Y%m")
+            )
+            redis_connection.lpush(key, json.dumps(pressure_data))
+        print("buffer length", len(pressure_buffer))
+        print(pressure_data)
+        pressure_buffer.clear()
+        buffer_time = utc_ceil_minute
+    elif utc_ceil_minute > buffer_time and len(pressure_buffer) == 0:
+        buffer_time = utc_ceil_minute
+    pressure_buffer.append(data["pressure"])
+    # add pressure values to altitude log if a significant pressure difference
+    # occurs which was not considered by the tracking log.
     if log_altitude and np.abs(np.diff([data["pressure"], old_pressure])) > 5:
-        key = "altitude:{}:{}".format(data["hostname"], strftime("%Y%m%d"))
+        key = "altitude:{}:{}".format(data["hostname"], time.strftime("%Y%m%d"))
         redis_connection.lpush(key, json.dumps(data))
         old_pressure = data["pressure"]
-    sleep(0.1)
+    time.sleep(0.1)
     continue
