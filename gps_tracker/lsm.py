4bdb97c4779ad8e2497aef8007ad552007379849
Jannis Lübbe <jannis@jl82.de>
2022-02-27 21:02:46 +0100

    renamed variables holding i2c adresses to be compatible with devices having multiple adresses. Switched off BDU. Added method to read out gyro data for some lsm devices.

--- gps_tracker/lsm.py
+++ gps_tracker/lsm.py
@@ -13,18 +13,25 @@ from ahrs.filters import Tilt
 class Lsm:
     def __init__(self):
         self.hostname = socket.gethostname()
+        self.GYR_ADDRESS = None
 
     def get_raw_acceleration(self):
         # in order to read multiple bytes the high bit of the sub address must be
         # asserted so we |0x80 to enable register auto-increment
         raw = self.bus.read_i2c_block_data(
-            self.i2c_address, self.OUT_X_L_A | 0x80, 6
+            self.ACC_ADDRESS, self.OUT_X_L_A | 0x80, 6
         )
         return list(struct.unpack("<hhh", bytearray(raw)))
 
     def get_raw_magnetometer(self):
         raw = self.bus.read_i2c_block_data(
-            self.i2c_address, self.OUT_X_L_M | 0x80, 6
+            self.MAG_ADDRESS, self.OUT_X_L_M | 0x80, 6
+        )
+        return list(struct.unpack("<hhh", bytearray(raw)))
+
+    def get_raw_gyro(self):
+        raw = self.bus.read_i2c_block_data(
+            self.GYR_ADDRESS, self.OUT_X_L_G | 0x80, 6
         )
         return list(struct.unpack("<hhh", bytearray(raw)))
 
@@ -33,7 +40,7 @@ class Lsm:
         """
         data = self.get_raw_acceleration()
         calibration = self.calibration
-        scaling = self.ACCEL_SCALE * calibration["g"] * 2 ** -15
+        scaling = self.ACCEL_SCALE * calibration["g"]
         corrected = [
             (data[0] - calibration["a_offset_x"]) * calibration["scale_a_x"],
             (data[1] - calibration["a_offset_y"]) * calibration["scale_a_y"],
@@ -45,8 +52,21 @@ class Lsm:
         """ returns magnetic flux density as [[x, y, z]] in units of mT.
         """
         data = self.get_raw_magnetometer()
-        scaling = self.MAG_SCALE * 2 ** -15
+        scaling = self.MAG_SCALE
         return np.array([data]) * scaling
+
+    def get_gyro(self):
+        """ returns angular velocity as [[x, y, z]] in units of dps.
+        """
+        data = self.get_raw_gyro()
+        calibration = self.calibration
+        scaling = self.GYR_SCALE
+        corrected = [
+            data[0] - calibration["a_offset_x"],
+            data[1] - calibration["a_offset_y"],
+            data[2] - calibration["a_offset_z"]
+        ]
+        return np.array([corrected]) * scaling
 
     def get_sensor_data(self):
         timestamp = time.time()
--- gps_tracker/lsm303d.py
+++ gps_tracker/lsm303d.py
@@ -6,7 +6,8 @@ import json
 import os
 from lsm import Lsm
 
-
+ACC_ADDRESS = 0x1D
+MAG_ADDRESS = 0x1D
 WHO_AM_I = 0x0F
 CTRL_REG0 = 0x1F
 CTRL_REG1 = 0x20
@@ -19,22 +20,22 @@ CTRL_REG7 = 0x26
 OUT_X_L_A = 0x28
 OUT_X_L_M = 0x08
 
-ACCEL_SCALE = 2  # +/- 2g
-MAG_SCALE = 0.2  # +/- mT
-
+ACCEL_SCALE = 6.1e-5  # +/- 2g full scale
+MAG_SCALE = 8.0e-6  # +/- 0.2 mT full scale
 
 class DeviceNotFound(IOError):
     pass
 
 
 class Lsm303d(Lsm):
-    def __init__(self, i2c_address=0x1D, config_path=None):
+    def __init__(self, config_path=None):
         super().__init__()
         self.ACCEL_SCALE = ACCEL_SCALE
         self.MAG_SCALE = MAG_SCALE
         self.OUT_X_L_A = OUT_X_L_A
         self.OUT_X_L_M = OUT_X_L_M
-        self.i2c_address = i2c_address
+        self.ACC_ADDRESS = ACC_ADDRESS
+        self.MAG_ADDRESS = MAG_ADDRESS
         self.initialize_sensor()
         # ensure that the sensor is really initialized to avoid false data
         self.initialize_sensor()
@@ -50,15 +51,15 @@ class Lsm303d(Lsm):
     def initialize_sensor(self):
         # Get I2C bus
         self.bus = smbus.SMBus(1)
-        whoami = self.bus.read_byte_data(self.i2c_address, WHO_AM_I)
+        whoami = self.bus.read_byte_data(self.ACC_ADDRESS, WHO_AM_I)
         if whoami == 0x49:
-            self.bus.write_byte_data(self.i2c_address, CTRL_REG1, 0x5F)
-            self.bus.write_byte_data(self.i2c_address, CTRL_REG2, 0xC0)
-            self.bus.write_byte_data(self.i2c_address, CTRL_REG3, 0x00)
-            self.bus.write_byte_data(self.i2c_address, CTRL_REG4, 0x00)
-            self.bus.write_byte_data(self.i2c_address, CTRL_REG5, 0x10)
-            self.bus.write_byte_data(self.i2c_address, CTRL_REG6, 0x00)
-            self.bus.write_byte_data(self.i2c_address, CTRL_REG7, 0x00)
+            self.bus.write_byte_data(self.ACC_ADDRESS, CTRL_REG1, 0x57)
+            self.bus.write_byte_data(self.ACC_ADDRESS, CTRL_REG2, 0xC0)
+            self.bus.write_byte_data(self.ACC_ADDRESS, CTRL_REG3, 0x00)
+            self.bus.write_byte_data(self.ACC_ADDRESS, CTRL_REG4, 0x00)
+            self.bus.write_byte_data(self.ACC_ADDRESS, CTRL_REG5, 0x10)
+            self.bus.write_byte_data(self.ACC_ADDRESS, CTRL_REG6, 0x00)
+            self.bus.write_byte_data(self.ACC_ADDRESS, CTRL_REG7, 0x00)
         else:
             raise DeviceNotFound(f"LSM303D not found on {self.i2c_address}")
         time.sleep(0.5)
