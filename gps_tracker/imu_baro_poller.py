a835bfb76f4c59f37baa7f4f9770b36744cedcd5
Jannis Lübbe <jannis@jl82.de>
2022-07-11 23:58:26 +0200

    added sensor fusion to combined poller for IMU and barometer.

--- gps_tracker/imu_baro_poller.py
+++ gps_tracker/imu_baro_poller.py
@@ -2,27 +2,41 @@
 import time
 import json
 import redis
+import numpy as np
+import numpy.linalg as la
 from lsm_poller import get_lsm_sensor
 from barometer_poller import get_barometer_sensor
+from algorithms import calculate_pressure_altitude, KalmanImuAltitude
 
 
 if __name__ == "__main__":
 
     redis_connection = redis.Redis()
-    interval = 0.08
+    interval = 0.04
     imu_sensor = get_lsm_sensor()
     if imu_sensor is None:
         exit()
     baro_sensor = get_barometer_sensor()
     if baro_sensor is None:
         exit()
+    kia = KalmanImuAltitude(sigma_a=0.5)
+    g = 9.80665
+    old_i_utc = None
     while True:
         t_start = time.time()
-        imu_data = imu_sensor.get_sensor_data()
-        print(imu_data)
-        redis_connection.publish("imu", json.dumps(imu_data))
         baro_data = baro_sensor.get_sensor_data()
-        print(baro_data)
+        imu_data = imu_sensor.get_sensor_data()
+        kalman_data = kia.kalman_step(
+            utc=imu_data["i_utc"],
+            h=calculate_pressure_altitude(baro_data["pressure"]),
+            h_err=0.06,
+            a=imu_data["vertical_acceleration"] - g,
+            a_err=0.05,
+        )
+        if baro_data is not None:
+            baro_data["kalman_altitude"] = kalman_data["altitude"]
+            baro_data["kalman_vertical_speed"] = kalman_data["vertical_speed"]
+        redis_connection.publish("imu", json.dumps(imu_data))
         redis_connection.publish("barometer", json.dumps(baro_data))
         dt = time.time() - t_start
         time.sleep(max(0, interval - dt))
