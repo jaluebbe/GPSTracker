a2dba5d622229e14bc7e1305f005987305be2707
Jannis Lübbe <jannis@jl82.de>
2022-07-14 22:05:23 +0200

    modified kalman_step to avoid output of the initial state.

--- gps_tracker/algorithms.py
+++ gps_tracker/algorithms.py
@@ -21,35 +21,37 @@ class KalmanImuAltitude:
         self.old_utc = None
 
     def kalman_step(self, utc, h, h_err, a=None, a_err=None):
-        if self.old_utc is not None:
-            t = utc - self.old_utc
-            # process noise matrix
-            Q = self.sigma_a**2 * np.array(
-                [
-                    [t**4 / 4, t**3 / 2, t**2 / 2],
-                    [t**3 / 2, t**2, t],
-                    [t**2 / 2, t, 1],
-                ]
-            )
-            # transition matrix
-            F = np.array([[1, t, 0.5 * t**2], [0, 1, t], [0, 0, 1]])
-            # prediction
-            x_pred = np.dot(F, self.x)
-            P_pred = np.dot(F, np.dot(self.P, F.T)) + Q
-            # update
-            if not None in (h, h_err, a, a_err):
-                z = np.array([[h], [a]])
-                H = np.array([[1, 0, 0], [0, 0, 1]])
-                R = np.diag([h_err**2, a_err**2])
-            elif not None in (h, h_err):
-                z = np.array([[h]])
-                H = np.array([[1, 0, 0]])
-                R = np.diag([h_err**2])
-            y = z - np.dot(H, x_pred)
-            S = np.dot(H, np.dot(P_pred, H.T)) + R
-            K = np.dot(P_pred, np.dot(H.T, la.inv(S, check_finite=False)))
-            self.x = x_pred + np.dot(K, y)
-            self.P = P_pred - np.dot(K, np.dot(S, K.T))
+        if self.old_utc is None:
+            self.old_utc = utc
+            return
+        t = utc - self.old_utc
+        # process noise matrix
+        Q = self.sigma_a**2 * np.array(
+            [
+                [t**4 / 4, t**3 / 2, t**2 / 2],
+                [t**3 / 2, t**2, t],
+                [t**2 / 2, t, 1],
+            ]
+        )
+        # transition matrix
+        F = np.array([[1, t, 0.5 * t**2], [0, 1, t], [0, 0, 1]])
+        # prediction
+        x_pred = np.dot(F, self.x)
+        P_pred = np.dot(F, np.dot(self.P, F.T)) + Q
+        # update
+        if not None in (h, h_err, a, a_err):
+            z = np.array([[h], [a]])
+            H = np.array([[1, 0, 0], [0, 0, 1]])
+            R = np.diag([h_err**2, a_err**2])
+        elif not None in (h, h_err):
+            z = np.array([[h]])
+            H = np.array([[1, 0, 0]])
+            R = np.diag([h_err**2])
+        y = z - np.dot(H, x_pred)
+        S = np.dot(H, np.dot(P_pred, H.T)) + R
+        K = np.dot(P_pred, np.dot(H.T, la.inv(S, check_finite=False)))
+        self.x = x_pred + np.dot(K, y)
+        self.P = P_pred - np.dot(K, np.dot(S, K.T))
         self.old_utc = utc
         return {
             "altitude": self.x[0, 0],
