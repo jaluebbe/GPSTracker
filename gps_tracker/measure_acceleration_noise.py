a4f3d0470f98bb2efcd2c1c5459880f52771ff75
Jannis Lübbe <jannis@jl82.de>
2022-07-14 23:28:11 +0200

    added measure_acceleration_noise.py .

---
+++ gps_tracker/measure_acceleration_noise.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import redis
+import json
+import numpy as np
+from collections import deque
+import time
+
+
+def measure_acceleration_noise(duration=60):
+    redis_connection = redis.Redis(decode_responses=True)
+    acceleration_history = deque()
+    _pubsub = redis_connection.pubsub()
+    _pubsub.subscribe("imu")
+    t_start = None
+    for item in _pubsub.listen():
+        if not item["type"] == "message":
+            continue
+        if item["channel"] == "imu":
+            _data = json.loads(item["data"])
+            acceleration_history.append(_data["vertical_acceleration"])
+            if t_start is None:
+                t_start = _data["i_utc"]
+            elif _data["i_utc"] > t_start + duration:
+                t_stop = _data["i_utc"]
+                break
+    print(
+        "### measured {} samples in {:.2f}s ###".format(
+            len(acceleration_history), t_stop - t_start
+        )
+    )
+    print(
+        "acc_mean = {:.3f} +/- {:.3f} m/s^2".format(
+            np.mean(acceleration_history), np.std(acceleration_history)
+        )
+    )
+
+
+while True:
+    measure_acceleration_noise()
