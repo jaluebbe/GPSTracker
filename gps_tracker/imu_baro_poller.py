c5c79cb2d9e93e000d50fe035ef79f357fe850c9
Jannis Lübbe <jannis@jl82.de>
2022-07-16 18:35:04 +0200

    added flag to indicate that imu_barometer data is available.

--- gps_tracker/imu_baro_poller.py
+++ gps_tracker/imu_baro_poller.py
@@ -52,9 +52,12 @@ if __name__ == "__main__":
             baro_data["imu_baro_pressure"] = calculate_altitude_pressure(
                 kalman_data["altitude"]
             )
+        imu_data["imu_barometer_available"] = True
+        baro_data["imu_barometer_available"] = True
         redis_connection.publish("imu", json.dumps(imu_data))
         redis_connection.publish("barometer", json.dumps(baro_data))
         imu_data.update(baro_data)
+        del imu_data["imu_barometer_available"]
         redis_connection.publish("imu_barometer", json.dumps(imu_data))
         dt = time.time() - t_start
         time.sleep(max(0, interval - dt))
