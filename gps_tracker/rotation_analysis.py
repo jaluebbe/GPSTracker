05e719f2af3c4cc5acee27ecf9ea16ad6ae53809
Jannis Lübbe <jannis@jl82.de>
2022-11-27 22:33:32 +0100

    added gyro calibration to startup of rotation_analysis.py .

--- gps_tracker/rotation_analysis.py
+++ gps_tracker/rotation_analysis.py
@@ -3,6 +3,7 @@ import json
 import redis
 import numpy as np
 import time
+import gyr_calibration
 
 
 def heading_diff(new_heading: float, old_heading: float) -> float:
@@ -14,22 +15,65 @@ def heading_diff(new_heading: float, old_heading: float) -> float:
     return difference
 
 
+class Compass:
+    def __init__(self, rotations: float = 0):
+        self.mag_x_min = 32768
+        self.mag_x_max = -32768
+        self.mag_y_min = 32768
+        self.mag_y_max = -32768
+        self.rotations = rotations
+        self._old_heading = None
+        self.heading = None
+
+    def set_mag_data(self, x, y):
+        self.mag_x_min = min(self.mag_x_min, x)
+        self.mag_x_max = max(self.mag_x_max, x)
+        self.mag_y_min = min(self.mag_y_min, y)
+        self.mag_y_max = max(self.mag_y_max, y)
+        self.heading = (
+            np.arctan2(
+                -(y - 0.5 * (self.mag_y_min + self.mag_y_max)),
+                x - 0.5 * (self.mag_x_min + self.mag_x_max),
+            )
+            / np.pi
+            * 180
+        )
+        if self._old_heading is not None:
+            self.rotations += (
+                heading_diff(self.heading, self._old_heading) / 360
+            )
+        self._old_heading = self.heading
+
+
+class Gyro:
+    def __init__(self, rotations: float = 0):
+        self.rotations = rotations
+        self.gyro_offset = 0
+        self._old_timestamp = None
+
+    def set_gyro_data(self, angular_rate, timestamp):
+        self.rpm = (angular_rate + self.gyro_offset) * 60 / (2 * np.pi)
+        if self._old_timestamp is not None:
+            dt = timestamp - self._old_timestamp
+            self.rotations += angular_rate * dt / (2 * np.pi)
+        self._old_timestamp = timestamp
+
+
 if __name__ == "__main__":
+    gyr_calibration.calibrate()
     redis_connection = redis.Redis(decode_responses=True)
     _pubsub = redis_connection.pubsub()
     _pubsub.subscribe("imu")
-    t_old = None
-    heading_old = None
     stored_turns = redis_connection.get("rotations")
     if stored_turns is None:
-        rotations = 0
+        old_rotations = 0
     else:
-        rotations = json.loads(stored_turns)
+        old_rotations = json.loads(stored_turns)
     stored_compass_turns = redis_connection.get("compass_rotations")
     if stored_compass_turns is None:
-        compass_rotations = 0
+        old_compass_rotations = 0
     else:
-        compass_rotations = json.loads(stored_compass_turns)
+        old_compass_rotations = json.loads(stored_compass_turns)
     stored_trips = redis_connection.get("trips")
     if stored_trips is None:
         trips = 0
@@ -37,77 +81,60 @@ if __name__ == "__main__":
         trips = json.loads(stored_trips)
     on_trip = False
     trip_start = None
-    min_trip_speed = 3
-    min_trip_duration = 10
+    min_trip_speed = 1
+    min_trip_duration = 120
     archive = False
     last_msg = None
-    old_rotations = rotations
-    old_compass_rotations = compass_rotations
-    mag_x_min = 0
-    mag_x_max = 0
-    mag_y_min = 0
-    mag_y_max = 0
+    gyro_offset = 0
+    compass = Compass(old_compass_rotations)
+    gyro = Gyro(old_rotations)
     for item in _pubsub.listen():
         if not item["type"] == "message":
             continue
         if item["channel"] == "imu":
             _data = json.loads(item["data"])
             angular_rate = _data["gyro"][2]
-            rpm = angular_rate * 60 / (2 * np.pi)
+            timestamp = _data["i_utc"]
+            gyro.set_gyro_data(angular_rate, timestamp)
             mag_x = _data["raw_magnetometer"][0]
             mag_y = _data["raw_magnetometer"][1]
-            mag_x_min = min(mag_x_min, mag_x)
-            mag_x_max = max(mag_x_max, mag_x)
-            mag_y_min = min(mag_y_min, mag_y)
-            mag_y_max = max(mag_y_max, mag_y)
-            heading = (
-                np.arctan2(
-                    -(mag_y - 0.5 * (mag_y_min + mag_y_max)),
-                    mag_x - 0.5 * (mag_x_min + mag_x_max),
-                )
-                / np.pi
-                * 180
-            )
+            compass.set_mag_data(mag_x, mag_y)
             if on_trip:
                 trip_duration = _data["i_utc"] - trip_start
-            if rpm > min_trip_speed and not on_trip:
+            if gyro.rpm > min_trip_speed and not on_trip:
                 on_trip = True
-                trip_start = _data["i_utc"]
+                trip_start = timestamp
                 trip_duration = 0
-            elif rpm < min_trip_speed and on_trip:
+            elif gyro.rpm < min_trip_speed and on_trip:
                 on_trip = False
                 if trip_duration > min_trip_duration:
                     trips += 1
                 archive = True
-            if t_old is not None:
-                dt = _data["i_utc"] - t_old
-                rotations += angular_rate * dt / (2 * np.pi)
-                if rotations > old_rotations + 1:
-                    redis_connection.set("rotations", json.dumps(rotations))
-                    old_rotations = rotations
-                compass_rotations += heading_diff(heading, heading_old) / 360
-                if compass_rotations > old_compass_rotations + 1:
-                    redis_connection.set(
-                        "compass_rotations", json.dumps(compass_rotations)
-                    )
-                    old_compass_rotations = compass_rotations
+            if gyro.rotations > old_rotations + 1:
+                redis_connection.set("rotations", json.dumps(gyro.rotations))
+                old_rotations = gyro.rotations
+            if compass.rotations > old_compass_rotations + 1:
+                redis_connection.set(
+                    "compass_rotations", json.dumps(compass.rotations)
+                )
+                old_compass_rotations = compass.rotations
             msg = {
-                "rotations": int(rotations),
-                "compass_rotations": int(compass_rotations),
-                "rpm": round(rpm, 3),
-                "heading": round(heading, 3),
+                "rotations": int(gyro.rotations),
+                "compass_rotations": int(compass.rotations),
+                "rpm": round(gyro.rpm, 3),
+                "heading": round(compass.heading, 3),
                 "trips": trips,
                 "on_trip": on_trip,
                 "trip_start": trip_start,
             }
             if on_trip:
                 msg["trip_duration"] = int(trip_duration)
-            if last_msg is None or _data["i_utc"] - last_msg > 0.3:
+            if last_msg is None or timestamp - last_msg > 0.3:
                 redis_connection.publish("rotation", json.dumps(msg))
-                last_msg = _data["i_utc"]
+                last_msg = timestamp
             if archive:
                 archive = False
-                msg["trip_stop"] = _data["i_utc"]
+                msg["trip_stop"] = timestamp
                 key = "rotation:{}:{}".format(
                     _data["i_hostname"], time.strftime("%Y%m%d")
                 )
@@ -116,5 +143,3 @@ if __name__ == "__main__":
                 redis_connection.lpush(key, json.dumps(msg))
                 print(json.dumps(msg))
                 redis_connection.set("trips", json.dumps(trips))
-            t_old = _data["i_utc"]
-            heading_old = heading
