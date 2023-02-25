c3de05f1e5316d38c2ef30ff69f4c82b4d68a7bb
Jannis Lübbe <jannis@jl82.de>
2023-02-25 17:35:33 +0100

    renamed wmsLayer to topPlusOpenLayer. Removed osmLayer hosted on OpenStreetMap servers. Adjusted default view and zoom level.

--- static/gps_visualisation_base.js
+++ static/gps_visualisation_base.js
@@ -1,20 +1,23 @@
-var map = L.map('map').setView([50.0, 8.0], 5);
+var map = L.map('map', {
+    minZoom: 7,
+    maxZoom: 19
+});
+map.setView([49.0, 9.0], 7);
 map.attributionControl.addAttribution(
     '<a href="https://github.com/jaluebbe/GPSTracker">Source on GitHub</a>');
 // add link to privacy statement
 //map.attributionControl.addAttribution(
 //    '<a href="static/datenschutz.html" target="_blank">Datenschutzerkl&auml;rung</a>');
-var wmsLayer = L.tileLayer.wms('https://sgx.geodatenzentrum.de/wms_topplus_open', {
+var topPlusOpenLayer = L.tileLayer.wms('https://sgx.geodatenzentrum.de/wms_topplus_open', {
     layers: 'web',
     format: 'image/png',
     transparent: true,
     minZoom: 1,
+    maxNativeZoom: 18,
+    maxZoom: 19,
     attribution: '&copy <a href="https://www.bkg.bund.de">BKG</a> 2019, ' +
         '<a href= "http://sg.geodatenzentrum.de/web_public/Datenquellen_TopPlus_Open.pdf" >data sources</a> '
 });
-var osmLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
 var otmLayer = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
     maxZoom: 15,
     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> ' +
@@ -59,9 +62,8 @@ L.control.scale({
     'imperial': false
 }).addTo(map);
 baseLayers = {
-    "TopPlusOpen": wmsLayer,
+    "TopPlusOpen": topPlusOpenLayer,
     "OpenTopoMap": otmLayer,
-    "OpenStreetMap": osmLayer,
     "Esri Imagery": esriImagery,
     "swiss map": SwissFederalGeoportal_NationalMapColor,
     "SWISSIMAGE": SwissFederalGeoportal_SWISSIMAGE
@@ -75,11 +77,4 @@ var layerControl = L.control.layers(baseLayers, otherLayers, {
     collapsed: L.Browser.mobile, // hide on mobile devices
     position: 'topright'
 }).addTo(map);
-var swissBounds = L.latLngBounds(L.latLng(45.6755, 5.7349), L.latLng(47.9163, 10.6677));
-map.on('baselayerchange', function(eo) {
-    if (eo.name === "swiss map" || eo.name === "SWISSIMAGE") {
-        if (!swissBounds.overlaps(map.getBounds())) {
-            map.fitBounds(swissBounds);
-        }
-    }
-});
+topPlusOpenLayer.addTo(map);
