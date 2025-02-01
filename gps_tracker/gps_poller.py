f99d25ce46f14d1f0df7a9963de65acede445423
Jannis Lübbe <jannisluebbe1982@gmail.com>
2025-02-01 23:33:36 +0100

    updated gps_poller.py to work withg venv.

--- gps_tracker/gps_poller.py
+++ gps_tracker/gps_poller.py
@@ -22,6 +22,7 @@ signal.signal(signal.SIGTERM, sigterm_handler)
 async def consume_gpsd():
     hostname = socket.gethostname()
     redis_connection = aioredis.Redis()
+
     async with gps.aiogps.aiogps() as gpsd:
         sky_info = {}
         config_counter = 0
@@ -29,13 +30,14 @@ async def consume_gpsd():
         old_utc = None
         async for msg in gpsd:
             if msg["class"] == "SKY":
-                sky_info.clear()
-                for _key in ("hdop", "vdop", "pdop", "uSat", "nSat"):
-                    sky_info[_key] = msg[_key]
+                sky_info = {
+                    key: msg[key]
+                    for key in ("hdop", "vdop", "pdop", "uSat", "nSat")
+                }
             elif msg["class"] == "TPV":
-                data = dict(msg)
-                if data.get("time") is None:
+                if "time" not in msg:
                     continue
+                data = dict(msg)
                 data.update(sky_info)
                 data["hostname"] = hostname
                 data["utc"] = (
@@ -51,26 +53,28 @@ async def consume_gpsd():
                 if data["mode"] > 1:
                     data["sensor"] = "gps"
                     await redis_connection.publish("gps", orjson.dumps(data))
-                if devices is not None and old_utc is not None:
+                if devices and old_utc:
                     _driver = devices[0]["driver"]
                     _path = devices[0]["path"]
-                    if config_counter > 5:
-                        pass
-                    elif _path != "/dev/serial0":
-                        pass
-                    elif _driver == "u-blox" and data["utc"] - old_utc > 0.7:
-                        # set the data rate of the GPS to 2Hz
-                        # (up to 10Hz is possible)
-                        subprocess.check_output(
-                            ["gpsctl", "-c", "0.5", "-s", "115200"], timeout=8
-                        )
-                        config_counter = config_counter + 1
-                    elif _driver == "MTK-3301" and data["utc"] - old_utc > 0.7:
-                        # set the data rate of the GPS to 2Hz
-                        subprocess.check_output(
-                            ["gpsctl", "-c", "0.5"], timeout=8
-                        )
-                        config_counter = config_counter + 1
+                    if (
+                        config_counter <= 5
+                        and _path == "/dev/serial0"
+                        and data["utc"] - old_utc > 0.7
+                    ):
+                        if _driver == "u-blox":
+                            # set the data rate of the GPS to 2Hz
+                            # (up to 10Hz is possible)
+                            subprocess.check_output(
+                                ["gpsctl", "-c", "0.5", "-s", "115200"],
+                                timeout=8,
+                            )
+                            config_counter += 1
+                        elif _driver == "MTK-3301":
+                            # set the data rate of the GPS to 2Hz
+                            subprocess.check_output(
+                                ["gpsctl", "-c", "0.5"], timeout=8
+                            )
+                            config_counter += 1
                 old_utc = data["utc"]
             elif msg["class"] == "DEVICES":
                 devices = msg["devices"]
