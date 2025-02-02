f79ae3c73ff736974f4b8433652de251231a1346
Jannis Lübbe <jannisluebbe1982@gmail.com>
2025-02-02 23:30:01 +0100

    migrated pressure_logger.py to venv. Added some checks to avoid crash if pressure data is missing.

--- gps_tracker/pressure_logger.py
+++ gps_tracker/pressure_logger.py
@@ -1,77 +1,97 @@
-#!/usr/bin/env python3
+#!venv/bin/python3
 import time
 import json
 from collections import deque
 import numpy as np
 import redis
 
-redis_connection = redis.Redis(decode_responses=True)
-pressure_buffer = deque(maxlen=700)
-buffer_time = None
-pressure_data = None
-_pubsub = redis_connection.pubsub()
-_pubsub.subscribe(["barometer", "transfer_data"])
-old_pressure = 0
-old_pressure_utc = 0
-log_altitude = True
-log_pressure = True
-log_pressure_minutes = [20, 50, 0, 10, 30, 40]
 
-for item in _pubsub.listen():
-    if not item[u"type"] == "message":
-        continue
-    # the last transfer_data message represents the last data dump to Redis.
-    if item["channel"] == "transfer_data":
-        _transfer_data = json.loads(item["data"])
-        if _transfer_data["utc"] > old_pressure_utc:
-            old_pressure = _transfer_data["pressure"]
-            old_pressure_utc = _transfer_data["utc"]
-        continue
-    data = json.loads(item["data"])
-    utc = data["p_utc"]
-    utc_ceil_minute = utc - utc % 60 + 60
-    next_minute = time.gmtime(utc_ceil_minute).tm_min
-    if buffer_time is None:
-        buffer_time = utc_ceil_minute
-    elif utc_ceil_minute > buffer_time and len(pressure_buffer) > 0:
-        pressure_data = {
-            "pressure": int(round(np.mean(pressure_buffer))),
-            "temperature": round(data["temperature"], 1),
-            "utc": int(buffer_time),
-            "hostname": data["p_hostname"],
-        }
-        if data.get("humidity") is not None:
-            pressure_data["humidity"] = int(round(data["humidity"]))
-        if (
-            log_pressure
-            and time.gmtime(buffer_time).tm_min in log_pressure_minutes
-        ):
-            key = "pressure:{}:{}".format(
-                data["p_hostname"], time.strftime("%Y%m")
+class PressureLogger:
+    def __init__(self):
+        self.redis_connection = redis.Redis(decode_responses=True)
+        self.pressure_buffer = deque(maxlen=700)
+        self.buffer_time = None
+        self.pressure_data = None
+        self.old_pressure = 0
+        self.old_pressure_utc = 0
+        self.log_altitude = True
+        self.log_pressure = True
+        self.log_pressure_minutes = {0, 10, 20, 30, 40, 50}
+        self._pubsub = self.redis_connection.pubsub()
+        self._pubsub.subscribe(["barometer", "transfer_data"])
+
+    def process_transfer_data(self, data):
+        if data["utc"] > self.old_pressure_utc:
+            self.old_pressure = data["pressure"]
+            self.old_pressure_utc = data["utc"]
+
+    def log_pressure_data(self, data):
+        key = f"pressure:{data['p_hostname']}:{time.strftime('%Y%m')}"
+        self.redis_connection.lpush(key, json.dumps(data))
+
+    def log_altitude_data(self, data):
+        key = f"altitude:{data['p_hostname']}:{time.strftime('%Y%m%d')}"
+        self.redis_connection.lpush(key, json.dumps(data))
+
+    def process_message(self, item):
+        if item["type"] != "message":
+            return
+        data = json.loads(item["data"])
+        if "pressure" not in data or "p_utc" not in data:
+            return
+        # The last transfer_data message represents the last data dump to Redis.
+        if item["channel"] == "transfer_data":
+            self.process_transfer_data(data)
+            return
+        utc = data["p_utc"]
+        utc_ceil_minute = utc - utc % 60 + 60
+        if self.buffer_time is None:
+            self.buffer_time = utc_ceil_minute
+        elif utc_ceil_minute > self.buffer_time and self.pressure_buffer:
+            self.pressure_data = {
+                "pressure": int(round(np.mean(self.pressure_buffer))),
+                "temperature": round(data["temperature"], 1),
+                "utc": int(self.buffer_time),
+                "hostname": data["p_hostname"],
+            }
+            if "humidity" in data:
+                self.pressure_data["humidity"] = int(round(data["humidity"]))
+
+            if (
+                self.log_pressure
+                and time.gmtime(self.buffer_time).tm_min
+                in self.log_pressure_minutes
+            ):
+                self.log_pressure_data(self.pressure_data)
+            self.pressure_buffer.clear()
+            self.buffer_time = utc_ceil_minute
+        elif utc_ceil_minute > self.buffer_time and not self.pressure_buffer:
+            self.buffer_time = utc_ceil_minute
+        self.pressure_buffer.append(data["pressure"])
+        # Add pressure values to altitude log if a significant pressure
+        # difference occurs which was not considered by the tracking log.
+        # Ignore small deviations from the mean value.
+        # Skip logging if no mean value is available, yet.
+        # Don't log more frequently than one second.
+        if self.pressure_data and data["p_utc"] >= self.old_pressure_utc + 1:
+            pressure_diff = abs(
+                data["pressure"] - self.pressure_data["pressure"]
             )
-            redis_connection.lpush(key, json.dumps(pressure_data))
-        pressure_buffer.clear()
-        buffer_time = utc_ceil_minute
-    elif utc_ceil_minute > buffer_time and len(pressure_buffer) == 0:
-        buffer_time = utc_ceil_minute
-    pressure_buffer.append(data["pressure"])
-    # Add pressure values to altitude log if a significant pressure difference
-    # occurs which was not considered by the tracking log.
-    # Ignore small deviations from the mean value.
-    # Skip logging if no mean value is available, yet.
-    # Don't log more frequently than one second.
-    if pressure_data is None:
-        pass
-    elif data["p_utc"] < old_pressure_utc + 1:
-        pass
-    elif np.abs(np.diff([data["pressure"], pressure_data["pressure"]])) < 10:
-        pass
-    elif (
-        log_altitude and np.abs(np.diff([data["pressure"], old_pressure])) > 10
-    ):
-        key = "altitude:{}:{}".format(data["p_hostname"], time.strftime("%Y%m%d"))
-        redis_connection.lpush(key, json.dumps(data))
-        old_pressure = data["pressure"]
-        old_pressure_utc = data["p_utc"]
-    time.sleep(0.05)
-    continue
+            if (
+                pressure_diff >= 10
+                and self.log_altitude
+                and abs(data["pressure"] - self.old_pressure) > 10
+            ):
+                self.log_altitude_data(data)
+                self.old_pressure = data["pressure"]
+                self.old_pressure_utc = data["p_utc"]
+
+    def run(self):
+        for item in self._pubsub.listen():
+            self.process_message(item)
+            time.sleep(0.05)
+
+
+if __name__ == "__main__":
+    logger = PressureLogger()
+    logger.run()
