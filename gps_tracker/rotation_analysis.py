d28aa5b8dc03a7242e31961a8b6a7d73eb196535
Jannis Lübbe <jannisluebbe1982@gmail.com>
2025-02-03 23:36:07 +0100

    migrated rotation_analysis.py to venv. Rearranged code in a class.

--- gps_tracker/rotation_analysis.py
+++ gps_tracker/rotation_analysis.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!venv/bin/python3
 import json
 import redis
 import numpy as np
@@ -59,87 +59,105 @@ class Gyro:
         self._old_timestamp = timestamp
 
 
+class RotationAnalysis:
+    def __init__(self):
+        self.redis_connection = redis.Redis(decode_responses=True)
+        self._pubsub = self.redis_connection.pubsub()
+        self._pubsub.subscribe("imu")
+        self.old_rotations = self._get_stored_value("rotations", 0)
+        self.old_compass_rotations = self._get_stored_value(
+            "compass_rotations", 0
+        )
+        self.trips = self._get_stored_value("trips", 0)
+        self.on_trip = False
+        self.trip_start = None
+        self.min_trip_speed = 1
+        self.min_trip_duration = 120
+        self.archive = False
+        self.last_msg = None
+        self.compass = Compass(self.old_compass_rotations)
+        self.gyro = Gyro(self.old_rotations)
+
+    def _get_stored_value(self, key, default):
+        stored_value = self.redis_connection.get(key)
+        return json.loads(stored_value) if stored_value is not None else default
+
+    def process_message(self, item):
+        if item["type"] != "message":
+            return
+
+        data = json.loads(item["data"])
+
+        if item["channel"] == "imu":
+            self.process_imu_data(data)
+
+    def process_imu_data(self, data):
+        angular_rate = data["gyro"][2]
+        timestamp = data["i_utc"]
+        self.gyro.set_gyro_data(angular_rate, timestamp)
+        mag_x = data["raw_magnetometer"][0]
+        mag_y = data["raw_magnetometer"][1]
+        self.compass.set_mag_data(mag_x, mag_y)
+
+        if self.on_trip:
+            trip_duration = data["i_utc"] - self.trip_start
+
+        if self.gyro.rpm > self.min_trip_speed and not self.on_trip:
+            self.on_trip = True
+            self.trip_start = timestamp
+            trip_duration = 0
+        elif self.gyro.rpm < self.min_trip_speed and self.on_trip:
+            self.on_trip = False
+            if trip_duration > self.min_trip_duration:
+                self.trips += 1
+            self.archive = True
+
+        if self.gyro.rotations > self.old_rotations + 1:
+            self.redis_connection.set(
+                "rotations", json.dumps(self.gyro.rotations)
+            )
+            self.old_rotations = self.gyro.rotations
+
+        if self.compass.rotations > self.old_compass_rotations + 1:
+            self.redis_connection.set(
+                "compass_rotations", json.dumps(self.compass.rotations)
+            )
+            self.old_compass_rotations = self.compass.rotations
+
+        msg = {
+            "rotations": int(self.gyro.rotations),
+            "compass_rotations": int(self.compass.rotations),
+            "rpm": round(self.gyro.rpm, 3),
+            "heading": round(self.compass.heading, 3),
+            "trips": self.trips,
+            "on_trip": self.on_trip,
+            "trip_start": self.trip_start,
+        }
+
+        if self.on_trip:
+            msg["trip_duration"] = int(trip_duration)
+
+        if self.last_msg is None or timestamp - self.last_msg > 0.3:
+            self.redis_connection.publish("rotation", json.dumps(msg))
+            self.last_msg = timestamp
+
+        if self.archive:
+            self.archive = False
+            msg["trip_stop"] = timestamp
+            key = f"rotation:{data['i_hostname']}:{time.strftime('%Y%m%d')}"
+            for _key in ("rpm", "on_trip", "heading"):
+                msg.pop(_key, None)
+            self.redis_connection.lpush(key, json.dumps(msg))
+            print(json.dumps(msg))
+            self.redis_connection.set("trips", json.dumps(self.trips))
+
+    def run(self):
+        for item in self._pubsub.listen():
+            self.process_message(item)
+            time.sleep(0.05)
+
+
 if __name__ == "__main__":
     gyr_calibration.calibrate()
-    redis_connection = redis.Redis(decode_responses=True)
-    _pubsub = redis_connection.pubsub()
-    _pubsub.subscribe("imu")
-    stored_turns = redis_connection.get("rotations")
-    if stored_turns is None:
-        old_rotations = 0
-    else:
-        old_rotations = json.loads(stored_turns)
-    stored_compass_turns = redis_connection.get("compass_rotations")
-    if stored_compass_turns is None:
-        old_compass_rotations = 0
-    else:
-        old_compass_rotations = json.loads(stored_compass_turns)
-    stored_trips = redis_connection.get("trips")
-    if stored_trips is None:
-        trips = 0
-    else:
-        trips = json.loads(stored_trips)
-    on_trip = False
-    trip_start = None
-    min_trip_speed = 1
-    min_trip_duration = 120
-    archive = False
-    last_msg = None
-    gyro_offset = 0
-    compass = Compass(old_compass_rotations)
-    gyro = Gyro(old_rotations)
-    for item in _pubsub.listen():
-        if not item["type"] == "message":
-            continue
-        if item["channel"] == "imu":
-            _data = json.loads(item["data"])
-            angular_rate = _data["gyro"][2]
-            timestamp = _data["i_utc"]
-            gyro.set_gyro_data(angular_rate, timestamp)
-            mag_x = _data["raw_magnetometer"][0]
-            mag_y = _data["raw_magnetometer"][1]
-            compass.set_mag_data(mag_x, mag_y)
-            if on_trip:
-                trip_duration = _data["i_utc"] - trip_start
-            if gyro.rpm > min_trip_speed and not on_trip:
-                on_trip = True
-                trip_start = timestamp
-                trip_duration = 0
-            elif gyro.rpm < min_trip_speed and on_trip:
-                on_trip = False
-                if trip_duration > min_trip_duration:
-                    trips += 1
-                archive = True
-            if gyro.rotations > old_rotations + 1:
-                redis_connection.set("rotations", json.dumps(gyro.rotations))
-                old_rotations = gyro.rotations
-            if compass.rotations > old_compass_rotations + 1:
-                redis_connection.set(
-                    "compass_rotations", json.dumps(compass.rotations)
-                )
-                old_compass_rotations = compass.rotations
-            msg = {
-                "rotations": int(gyro.rotations),
-                "compass_rotations": int(compass.rotations),
-                "rpm": round(gyro.rpm, 3),
-                "heading": round(compass.heading, 3),
-                "trips": trips,
-                "on_trip": on_trip,
-                "trip_start": trip_start,
-            }
-            if on_trip:
-                msg["trip_duration"] = int(trip_duration)
-            if last_msg is None or timestamp - last_msg > 0.3:
-                redis_connection.publish("rotation", json.dumps(msg))
-                last_msg = timestamp
-            if archive:
-                archive = False
-                msg["trip_stop"] = timestamp
-                key = "rotation:{}:{}".format(
-                    _data["i_hostname"], time.strftime("%Y%m%d")
-                )
-                for _key in ("rpm", "on_trip", "heading"):
-                    msg.pop(_key, None)
-                redis_connection.lpush(key, json.dumps(msg))
-                print(json.dumps(msg))
-                redis_connection.set("trips", json.dumps(trips))
+    rotation_analysis = RotationAnalysis()
+    rotation_analysis.run()
