4239dd9f73c87fbd816d950b1fd6eb62eb842202
Jannis Lübbe <jannisluebbe1982@gmail.com>
2025-02-23 12:25:06 +0100

    updated lsm_poller.py to startup from venv and to use a separate main method.

--- gps_tracker/lsm_poller.py
+++ gps_tracker/lsm_poller.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!venv/bin/python3
 import time
 import json
 import redis
@@ -8,6 +8,7 @@ from lsm6dsl_lis3mdl import Lsm6dsl_Lis3mdl
 
 
 def get_lsm_sensor():
+    """Attempt to initialize each IMU sensor in turn."""
     try:
         return Lsm303d()
     except:
@@ -22,16 +23,20 @@ def get_lsm_sensor():
         print("no LSM6DSL+LIS3MDL found")
 
 
-if __name__ == "__main__":
-
+def main():
     redis_connection = redis.Redis()
     interval = 0.08
     sensor = get_lsm_sensor()
     if sensor is None:
-        exit()
+        print("No IMU sensor found. Exiting.")
+        return
     while True:
         t_start = time.time()
         sensor_data = sensor.get_sensor_data(sensor_fusion=False)
         redis_connection.publish("imu", json.dumps(sensor_data))
         dt = time.time() - t_start
         time.sleep(max(0, interval - dt))
+
+
+if __name__ == "__main__":
+    main()
